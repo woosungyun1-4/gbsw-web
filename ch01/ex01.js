6da5dc86cc9bdea6ae328c6a5f0798ce579746d2
woosungyun <helyangigame@gmail.com>
2026-03-04 13:44:49 +0900

    Update ex01.js

--- ch01/ex01.js
+++ ch01/ex01.js
@@ -1,11 +1,19 @@
-const a = 10;//const를 활용하여 a에 10저장
+const a = 10;// const를 활용하여 a에 10으로 초기화
+let b = 20;// let을 활용하여 b에 20으로 초기화
 
-console.log(a)//콘솔창에 a라는 변수 출력
+console.log(a);// 콘솔창에 a라는 변수 출력
+
+b = 30;// let을 활용하여 b에 30으로 초기화
+
+console.log(b);
 
 /*
 const
-변수 재선언 불가능
-블록 범위 
+상수형 블록 범위 변수
 
+let
+블록 범위 변수
 
+console.log
+콘솔창에 ()안에 있는거 출력
 */
